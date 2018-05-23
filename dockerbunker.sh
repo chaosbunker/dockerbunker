8d68e99c81a850914e7c34b650e3fd859155f0c4
Dennis Rodewyk <ufo@chaosbunker.com>
2018-05-23 23:27:06 +0000

    Check for presence of docker under CentOS, Fedora, and RHEL

--- dockerbunker.sh
+++ dockerbunker.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-if ! dpkg -l docker &>/dev/null;then
-	echo -e "\n\e[3m\xe2\x86\x92 \e[1mCould not find docker.\e[3m\n\nMost systems can install Docker by running:\n\nwget -qO- https://get.docker.com/ | sh\n";
-	exit 1
+if [[ -r /etc/redhat-release ]];then
+	if ! dnf list installed docker-ce  &>/dev/null;then
+	docker_missing=1
+	fi
+elif [[ -r /etc/debian_version ]];then
+	if ! dpkg -l docker &>/dev/null;then
+	docker_missing=1
+	fi
 fi
+
+[[ $docker_missing ]] \
+	&& echo -e "\n\e[3m\xe2\x86\x92 \e[1mCould not find docker.\e[3m\n\nMost systems can install Docker by running:\n\nwget -qO- https://get.docker.com/ | sh\n";
 
 # Find base dir
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
