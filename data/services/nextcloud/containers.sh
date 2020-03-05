15ae7b767bf01f30abbd619566dc819f01cefaa1
Dennis Rodewyk <chaosbunker@code.chaosbunker.com>
2020-03-05 13:37:26 +0100

    Set correct nextcloud db volume name

--- data/services/nextcloud/containers.sh
+++ data/services/nextcloud/containers.sh
@@ -4,7 +4,7 @@ nextcloud_db_dockerbunker() {
 		--restart=always \
 		--network dockerbunker-${SERVICE_NAME} --net-alias=db \
 		--env-file="${SERVICE_ENV}" \
-		-v ${SERVICE_NAME}-data-vol-1:${volumes[${SERVICE_NAME}-data-vol-1]} \
+		-v ${SERVICE_NAME}-db-vol-1:${volumes[${SERVICE_NAME}-db-vol-1]} \
 		-v "${SERVICES_DIR}"/${SERVICE_NAME}/mysql/:/etc/mysql/conf.d/:ro \
 		--health-cmd="mysqladmin ping --host localhost --silent" --health-interval=10s --health-retries=5 --health-timeout=30s \
 	${IMAGES[db]} >/dev/null
