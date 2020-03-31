c06b0e1d2d9b95074798e14cd6e381a5d4d13ccd
pukoo <entwicker@pukoo.de>
2020-03-31 17:42:12 +0200

    [TODO] SH, update service dronci to work with gittea and gogs

--- data/services/droneci/service.sh
+++ data/services/droneci/service.sh
@@ -21,27 +21,71 @@ configure() {
 
 	set_domain
 
-	read -p "Gogs server address: " -ei "https://" DRONE_GOGS_SERVER
+	# docker config
 
-	read -p "Gogs username: " -ei "" GOGS_USER
+	# git server specific config
+	PS3='Select your Server: '
+	options=("Gogs" "Gitea" "Quit")
+	select opt in "${options[@]}"
+	do
+		case $opt in
+			"Gogs")
+				CHOOSEN_SERVER_NAME="GOGS"
+				SAVE_GOGS_ENV_DATA=1
+				break
+			;;
+			"Gitea")
+				CHOOSEN_SERVER_NAME="GITEA"
 
-	cat <<-EOF >> "${SERVICE_ENV}"
-	PROPER_NAME="${PROPER_NAME}"
-	SERVICE_NAME=${SERVICE_NAME}
-	SSL_CHOICE=${SSL_CHOICE}
-	LE_EMAIL=${LE_EMAIL}
+				read -p "$CHOOSEN_SERVER_NAME oauth Client ID: " -ei "" DRONE_GITEA_CLIENT_ID
+				read -p "$CHOOSEN_SERVER_NAME  oauth Client Secret: " -ei "" DRONE_GITEA_CLIENT_SECRET
+				SAVE_GITEA_ENV_DATA=1
+				break
+			;;
+			"Quit")
+				break
+			;;
+			*)
+				echo "invalid option $REPLY"
+			;;
+		esac
+	done
 
-	DRONE_GOGS_SERVER=${DRONE_GOGS_SERVER}
-	DRONE_SERVER_HOST=${SERVICE_DOMAIN}
-	DRONE_SERVER_PROTO=https
-	DRONE_RUNNER_CAPACITY=2
-	DRONE_TLS_AUTOCERT=false
-	DRONE_GIT_ALWAYS_AUTH=false
-	DRONE_USER_CREATE=username:${GOGS_USER},admin:true
-	DRONE_DATABASE_SECRET=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32)
-	SERVICE_DOMAIN=${SERVICE_DOMAIN}
-	DRONE_AGENTS_DISABLED=true
-	EOF
+	if [ ${SAVE_GOGS_ENV_DATA} ] || [ ${SAVE_GITEA_ENV_DATA} ];then
+		read -p "$CHOOSEN_SERVER_NAME server address: " -ei "https://" DRONE_SERVER_ADDRESS
+
+		read -p "Drone-Ci username: " -ei "" DRONE_SERVER_USER
+
+		# save Drone-Ci Config
+		cat <<-EOF >> "${SERVICE_ENV}"
+		PROPER_NAME=${PROPER_NAME}
+		SERVICE_NAME=${SERVICE_NAME}
+		SSL_CHOICE=${SSL_CHOICE}
+		LE_EMAIL=${LE_EMAIL}
+
+		# Drone-Ci Server Config
+		DRONE_${CHOOSEN_SERVER_NAME}_SERVER=${DRONE_SERVER_ADDRESS}
+		DRONE_SERVER_HOST=${SERVICE_DOMAIN}
+		DRONE_SERVER_PROTO=https
+		DRONE_RUNNER_CAPACITY=2
+		DRONE_TLS_AUTOCERT=false
+		DRONE_GIT_ALWAYS_AUTH=false
+		DRONE_USER_CREATE=username:${DRONE_SERVER_USER},admin:true
+		DRONE_DATABASE_SECRET=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32)
+		SERVICE_DOMAIN=${SERVICE_DOMAIN}
+		DRONE_AGENTS_DISABLED=true
+		EOF
+
+		# save Drone-Ci special Gittea Config
+		if [ ${SAVE_GITEA_ENV_DATA} ];then
+			cat <<-EOF >> "${SERVICE_ENV}"
+
+			# Drone-Ci special Gittea Config
+			DRONE_GITEA_CLIENT_ID=${DRONE_GITEA_CLIENT_ID}
+			DRONE_GITEA_CLIENT_SECRET=${DRONE_GITEA_CLIENT_SECRET}
+			EOF
+		fi
+	fi
 
 	post_configure_routine
 }
