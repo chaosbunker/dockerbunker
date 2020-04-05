9b1eea06ead73e9e89ae7ae589f917dc0dec50a6
pukoo <entwicker@pukoo.de>
2020-04-05 02:20:45 +0200

    [TODO] exclude shell-script call parameter code to decrease duplicates

--- data/services/firefoxsync/init.sh
+++ data/services/firefoxsync/init.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 ######
 # this file is identical to other service files and should not be edited
 # pleas edit your service within service.sh
@@ -13,7 +12,7 @@ SERVICE_NAME="$(basename $(dirname "$BASH_SOURCE"))"
 PROMPT_SSL=1
 
 # load prior saved dockerbunker and service specific environment variables
-declare -a environment=( "build/env/dockerbunker.env" "data/include/init.sh" )
+declare -a environment=( "data/env/dockerbunker.env" "data/include/init.sh" )
 
 for env in "${environment[@]}";do
 	[[ -f "${BASE_DIR}"/$env ]] && source "${BASE_DIR}"/$env
@@ -22,16 +21,5 @@ done
 # load service specific configuration
 source "$(dirname "$BASH_SOURCE")/service.sh"
 
-# check shell-script call parameter
-if [[ -z $1 ]]; then
-  # if there isno parameter run menu function
-  options_menu
-elif [[ $1 == "letsencrypt" ]];then
-  # run letsencrypt function with given parameter
-  # e.g. init.sh letsencrypt issue
-	$1 $*
-else
-  # run other given functions as parameter
-  # e.g. init.sh reconfigure
-	$1
-fi
+# load common service function after service.sh
+source "$BASE_DIR/data/include/init_service_after.sh"
